36d30245ba771a9da5c78a1ce99d8b8b8fd94685
Mario Tijerino <mariogotijerino@gmail.com>
2021-02-08 20:19:51 -0700

    Columns and stair bricks

---
+++ Great Seal Recreation/Assets/Scripts/ImageOpacity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ImageOpacity : MonoBehaviour
+{
+    public Image image;
+    public Color color = Color.white;
+    private void Update()
+    {
+        if(image == null)
+        {
+            image = GetComponent<Image>();
+        }
+        color.a = Mathf.PingPong(Time.time, 1f);
+
+        image.color = color;
+    }
+}

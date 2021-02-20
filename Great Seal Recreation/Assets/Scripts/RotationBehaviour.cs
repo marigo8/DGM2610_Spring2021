8112383e12c6838824a2255bb9dbeb1c0744151b
Mario Tijerino <mariogotijerino@gmail.com>
2021-02-19 18:40:18 -0700

    Pyramid and Tympanum (Corrupt File)

---
+++ Great Seal Recreation/Assets/Scripts/RotationBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotationBehaviour : MonoBehaviour
+{
+    public Vector3 rotateSpeed;
+
+    private void Update()
+    {
+        transform.localEulerAngles += rotateSpeed * Time.deltaTime;
+    }
+}

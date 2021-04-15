b424f1e4f9bec37bd0f517c2b203c33eb7cfdffa
Mario Tijerino <mariogotijerino@gmail.com>
2021-04-15 11:54:57 -0600

    Fire and Plant Shader

---
+++ Great Seal Recreation/Assets/Scripts/LightFlickerBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightFlickerBehaviour : MonoBehaviour
+{
+    public Gradient flickerPattern;
+    public float flickerMultiplier;
+
+    private Light lightComp;
+
+    private float baseIntensity;
+    private float step;
+
+    private void Start()
+    {
+        lightComp = GetComponent<Light>();
+        baseIntensity = lightComp.intensity;
+    }
+
+    private void Update()
+    {
+        step += Time.deltaTime;
+        step %= 1;
+
+        var intensity = flickerPattern.Evaluate(step).r;
+
+        lightComp.intensity = baseIntensity + intensity * flickerMultiplier;
+    }
+}

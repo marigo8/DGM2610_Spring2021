1a6159d88f241d2bf0114c8da6a7b0ed583593db
Mario Tijerino <mariogotijerino@gmail.com>
2021-04-19 09:49:18 -0600

    Smoke, Mist, 3D Clouds

---
+++ Great Seal Recreation/Assets/Scripts/CloudCreateBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCreateBehaviour : MonoBehaviour
+{
+    public GameObject prefab;
+    public int layerCount;
+    public float spread;
+
+    public void Start()
+    {
+        for(var i = 0; i < layerCount; i++)
+        {
+            var position = transform.position;
+            position.y += (i*1f) / layerCount * spread;
+
+            var layer = Instantiate(prefab, position, transform.rotation);
+            var mat = layer.GetComponent<MeshRenderer>().material;
+
+            var baseValue = mat.GetVector("Vector2_5B0DD8CC");
+
+            var step = Mathf.PingPong(((i * 1f) / layerCount) * 2, 1);
+            baseValue.x = Mathf.Lerp(1f, baseValue.x, step);
+            baseValue.x = Mathf.Clamp(baseValue.x, 0, .9f);
+
+            mat.SetVector("Vector2_5B0DD8CC", baseValue);
+        }
+    }
+}
---
+++ Great Seal Recreation/Assets/Scripts/VolumeCloudBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeCloudBehaviour : MonoBehaviour
+{
+    private Material mat;
+    public float thickness;
+
+    private void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+
+        var defaultVector = mat.GetVector("Vector2_9D11B64");
+        var offset = transform.localPosition.z * thickness;
+        offset = Mathf.Abs(offset);
+        Debug.Log(offset);
+        Debug.Log(defaultVector);
+        defaultVector.x += offset;
+        mat.SetVector("Vector2_9D11B64", defaultVector);
+    }
+}
